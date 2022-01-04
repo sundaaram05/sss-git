53970822192c190a107cf4c042ab88bc92f1b6a7
sundaaram05 <97014093+sundaaram05@users.noreply.github.com>
2022-01-04 16:16:20 +0530

    Update Calculate.java
    View has changed

--- Calc/src/com/calc/view/Calculate.java
+++ Calc/src/com/calc/view/Calculate.java
@@ -1,54 +1,13 @@
 package com.calc.view;
 
-public class Calculate  {
+import com.calc.controller.CalcController;
+import com.calc.controller.CalcInterface;
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String d=null;
-		do{
-		System.out.println("enter two number");
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		System.out.println("enter any operator");
-		char c = sc.next().charAt(0);
-		switch(c)
-		{
-		case'+':
-		{
-		//method name
-		break;
-		}
-		case'-':
-		{
-		//method name
-		break;
-		}
-		case'*':
-		{
-			//method name
-		break;
-		}
-		case'/':
-		{
-			//method name
-		break;
-		}
-		case'%':
-		{
-			//method name
-		break;
-		}
-		default:
-		{
-		System.out.println("invalid oprator\n");
-		}
-		}
-		System.out.println("Do you want to continue press  Y or y");
-		System.out.println("to exit press any key other than y");
-				d =sc.next();
-				}while(d.equals("y") || d.equals("Y"));
-				System.out.println("Exited from application.");
-				
-		}
-
+public class Calculate {
+	public static void main(String args[]) {
+		CalcInterface ec = new CalcController();
+		
+		ec.calcTotal();
 	}
+
+}
