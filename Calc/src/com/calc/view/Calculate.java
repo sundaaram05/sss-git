a8c7623e44b5600c266011a90d0d298ceb45ba91
mayirikrishna <97013995+mayirikrishna@users.noreply.github.com>
2022-01-04 16:54:40 +0530

    Update Calculate.java
    edited

--- Calc/src/com/calc/view/Calculate.java
+++ Calc/src/com/calc/view/Calculate.java
@@ -6,7 +6,8 @@ import com.calc.controller.CalcInterface;
 public class Calculate {
 	public static void main(String args[]) {
 		System.out.println("Edited by prameela");
-		System.out.println("edit by manichand")
+		System.out.println("edit by manichand");
+		System.out.println("edit by siva krishna");
 		CalcInterface ec = new CalcController();
 		
 		ec.calcTotal();
