aa3fe0e28c951fc30da2427c89c25edc19c87c29
manamprameela <97014170+manamprameela@users.noreply.github.com>
2022-01-04 16:47:33 +0530

    Update Calculate.java
    added sopln

--- Calc/src/com/calc/view/Calculate.java
+++ Calc/src/com/calc/view/Calculate.java
@@ -5,6 +5,7 @@ import com.calc.controller.CalcInterface;
 
 public class Calculate {
 	public static void main(String args[]) {
+		System.out.println("Edited by prameela");
 		CalcInterface ec = new CalcController();
 		
 		ec.calcTotal();
