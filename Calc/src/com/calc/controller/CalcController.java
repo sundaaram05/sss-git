12c405087525ab7dda79e429631b38160d5e643f
manamprameela <97014170+manamprameela@users.noreply.github.com>
2022-01-04 15:45:44 +0530

    Update CalcController.java
    edited calcontroller

--- Calc/src/com/calc/controller/CalcController.java
+++ Calc/src/com/calc/controller/CalcController.java
@@ -1,5 +1,54 @@
 package com.calc.controller;
 
 public class CalcController {
-
+import com.calc.model;
+import java.util.Scanner; 
+public void add() 
+	 {
+	 Scanner sc = new Scanner(System.in);
+	 
+	 System.out.println("Enter first number");
+	 int num1= sc.nextInt();
+	 System.out.println("Enter second number");
+	 int num2= sc.nextInt();
+	 //System.out.println("Enter first number");
+	 float res= num1+num2;
+	 System.out.println("The addition result is "+res);
+	 }
+	 
+	 public void substract() 
+	 {
+	 Scanner sc = new Scanner(System.in);
+	 
+	 System.out.println("Enter first number");
+	 int num1= sc.nextInt();
+	 System.out.println("Enter second number");
+	 int num2= sc.nextInt();
+    float res= num1-num2;
+	 System.out.println("The substraction result is "+res);
+	 }
+	 
+	 public void mult() 
+	 {
+	 Scanner sc = new Scanner(System.in);
+	 
+	 System.out.println("Enter first number");
+	 int num1= sc.nextInt();
+	 System.out.println("Enter second number");
+	 int num2= sc.nextInt();
+     float res= num1*num2;
+	 System.out.println("The multiplication result is "+res);
+	 }
+	 
+	 public void divide() 
+	 {
+	 Scanner sc = new Scanner(System.in);
+	 
+	 System.out.println("Enter first number");
+	 int num1= sc.nextInt();
+	 System.out.println("Enter second number");
+	 int num2= sc.nextInt();
+	 float res= num1/num2;
+	 System.out.println("The division result is "+res);
+	 }
 }
